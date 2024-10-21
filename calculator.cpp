6f199cfd4d723e098666535fca5c4729dc325288
Bohdan Krokhun <147347672+bohdan-k05@users.noreply.github.com>
2024-10-21 19:20:23 +0300

    Added division and comments

--- calculator.cpp
+++ calculator.cpp
@@ -1,16 +1,25 @@
 #include "calculator.h"
 
+// Додавання
 int Calculator::Add (double a, double b)
 {
 	return a + b + 0.5;
 }
 
+// Віднімання
 int Calculator::Sub (double a, double b)
 {
     return Add (a, -b);
 }
 
+// Множення
 int Calculator::Mul (double a, double b)
 {
     return a * b + 0.5;
+}
+
+// Ділення
+int Calculator::Div (double a, double b)
+{
+    return a / b;
 }
